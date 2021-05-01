55e41b6ded3137fb0d2fb909e521729e25661c82
Salih Genc <salihgenc34@gmail.com>
2021-05-01 22:09:16 +0300

    odev tamamlandi.

--- src/App.js
+++ src/App.js
@@ -1,67 +1,73 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import { products } from "./constants";
 import ProductsGrid from "./components/ProductsGrid";
 import Topbar from "./components/Topbar/Topbar";
 
-class App extends Component {
-  state = {
-    cart: [
-      { id: 0, value: 0 },
-      { id: 1, value: 0 },
-      { id: 2, value: 0 },
-      { id: 3, value: 0 },
-      { id: 4, value: 0 },
-      { id: 5, value: 0 },
-    ],
-    itemCount: 0,
+
+
+ const App = () => {
+  
+  const [itemCount , setItemCount] = useState(0);
+  const [cart, setCart] = useState([
+    { id: 0, value: 0 },
+    { id: 1, value: 0 },
+    { id: 2, value: 0 },
+    { id: 3, value: 0 },
+    { id: 4, value: 0 },
+    { id: 5, value: 0 }]);
+
+
+  const  handleIncrement = product => {
+      // cart array'inin kopyasını oluştur
+      const updatedCart = [...cart];
+      // parametre olarak gelen product'ın cart array'i içerisindeki index'ini bul
+      const index = updatedCart.indexOf(product);
+      // kopyalanan cart array'ine bu ürünü ekle ve value property'sini 1 artır
+      updatedCart[index] = { ...updatedCart[index] };
+      updatedCart[index].value++;
+      // getItemCount fonksiyonunu kullanarak sepetteki ürün sayısını bul
+      const itemCount = getItemCount(updatedCart);
+      // state'i güncelle
+      setCart(cart);
+      setItemCount(itemCount);
+
   };
 
-  handleIncrement = (product) => {
-    // cart array'inin kopyasını oluştur
-    const cart = [...this.state.cart];
-    // parametre olarak gelen product'ın cart array'i içerisindeki index'ini bul
-    const index = cart.indexOf(product);
-    // kopyalanan cart array'ine bu ürünü ekle ve value property'sini 1 artır
-    cart[index] = { ...cart[index] };
-    cart[index].value++;
-    // getItemCount fonksiyonunu kullanarak sepetteki ürün sayısını bul
-    const itemCount = this.getItemCount(cart);
-    // state'i güncelle
-    this.setState({ cart, itemCount });
+
+  const handleDecrement = product => {
+    const updatedCart = [...cart];
+    const index = updatedCart.indexOf(product);
+    updatedCart[index] = { ...updatedCart[index] };
+    updatedCart[index].value--;
+    const itemCount = getItemCount(updatedCart);
+    setCart(cart);
+    setItemCount(itemCount);
   };
 
-  handleDecrement = (product) => {
-    const cart = [...this.state.cart];
-    const index = cart.indexOf(product);
-    cart[index] = { ...cart[index] };
-    cart[index].value--;
-    const itemCount = this.getItemCount(cart);
-    this.setState({ cart, itemCount });
-  };
-
-  getItemCount = (cart) => {
+  
+  const getItemCount = cart => {
     // Sepetteki toplam ürün sayısını bul
     let itemCount = cart.reduce((total, product) => total + product.value, 0);
 
     return itemCount;
   };
 
-  render() {
-    return (
-      <div className="App">
+
+  return ( 
+    <div className="App"> 
         <Topbar/>
-        <Navbar totalItems={this.state.itemCount} />
+        <Navbar totalItems={itemCount} />
         <ProductsGrid
           products={products}
-          cart={this.state.cart}
-          onIncrement={this.handleIncrement}
-          onDecrement={this.handleDecrement}
-        />
-      </div>
-    );
-  }
+          cart={cart}
+          onIncrement={handleIncrement}
+          onDecrement={handleDecrement}
+        /> 
+    </div>
+  )
 }
 
-export default App;
+export default App ;
+
